72f882baa5d8a15e77fe1d392785d05918eef28b
donghualin <donghualin@uniontech.com>
2022-04-06 11:43:39 +0800

    fix: 修复多网卡情况下点击控制中心无线加密网络无法唤起网络列表的问题
    增加网络列表扩展项是否显示的判断,只要在显示的情况下才断开连接并隐藏
    
    Log:
    Influence: 存在多个无线网卡,第一个网卡已连接网络,第二个网卡点击加密网络,查看任务栏网络列表是否弹出
    Bug: https://pms.uniontech.com/bug-view-119727.html
    Change-Id: Ia4be95b4286d9f04b68e78078434f3d2ac4899c3
    (cherry picked from commit 12b56488626678a91487abbf344d70a0142f4920)

--- dde-network-dialog/localclient.cpp
+++ dde-network-dialog/localclient.cpp
@@ -55,16 +55,18 @@ LocalClient::LocalClient(QObject *parent)
     connect(m_clinet, SIGNAL(connected()), this, SLOT(connectedHandler()));
     connect(m_clinet, SIGNAL(disconnected()), this, SLOT(disConnectedHandler()));
     connect(m_clinet, SIGNAL(readyRead()), this, SLOT(readyReadHandler()));
-    connect(m_exitTimer, &QTimer::timeout, this, [ this ] {
-        delete this;
-        qApp->quit();
-    });
+    connect(m_exitTimer, SIGNAL(timeout()), qApp, SLOT(quit()));
     ConnectToServer();
     updateTranslator(QLocale::system().name());
+
+    connect(qApp, &QApplication::aboutToQuit, this, [ this ] {
+        delete this;
+    });
 }
 
 LocalClient::~LocalClient()
 {
+    changePassword("","",false);
     m_clinet->disconnectFromServer();
     delete m_clinet;
     if (m_panel)
@@ -173,10 +175,7 @@ void LocalClient::initWidget()
         m_popopWindow->setProperty("localpos", QPoint(-1, -1));
         m_panel = new NetworkPanel(m_popopWindow);
         QObject::connect(qApp, &QCoreApplication::destroyed, m_popopWindow, &DockPopupWindow::deleteLater);
-        QObject::connect(m_popopWindow, &DockPopupWindow::hideSignal, this, [ this ] {
-            delete this;
-            qApp->quit();
-        });
+        QObject::connect(m_popopWindow, &DockPopupWindow::hideSignal, qApp, &QCoreApplication::quit);
         QObject::connect(m_popopWindow, &DockPopupWindow::hideSignal, m_popopWindow, &DockPopupWindow::deleteLater);
         QObject::connect(m_panel, &NetworkPanel::updateFinished, this, [ this ] {
             m_popopNeedShow = true;
@@ -263,17 +262,22 @@ void LocalClient::receivePassword(QLocalSocket *socket, const QByteArray &data)
     if (m_wait == WaitClient::Self) {
         QJsonDocument doc = QJsonDocument::fromJson(data);
         if (doc.isObject()) {
+            int exitCode = -4;
             QFile file;
-            if (!file.open(stdout, QFile::WriteOnly)) {
-                qInfo() << "open STDOUT failed";
-                qApp->exit(-4);
-            } else {
+            if (file.open(stdout, QFile::WriteOnly)) {
                 file.write("\npassword:" + data + "\n");
                 file.flush();
                 file.close();
                 m_ssid.clear();
-                qApp->exit(doc.object().value("input").toBool() ? 0 : 1);
+                exitCode = (doc.object().value("input").toBool() ? 0 : 1);
+            } else {
+                qInfo() << "open STDOUT failed";
             }
+            file.write("\npassword:" + data + "\n");
+            file.flush();
+            file.close();
+            m_ssid.clear();
+            qApp->exit(exitCode);
         }
     }
 }
