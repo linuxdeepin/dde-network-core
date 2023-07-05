7c7202476becbc681336d74ff8cbe85453ddf87b
heyuming <heyuming@uniontech.com>
2023-07-05 09:54:05 +0800

    chore: add translation for hotspot plugin
    Log: 为热点插件添加翻译

---
+++ dock-hotspot-plugin/translations/dock-hotspot-plugin_en_US.ts
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>HotspotPlugin</name>
+    <message>
+        <source>hotspot</source>
+        <translation>hotspot</translation>
+    </message>
+    <message>
+        <source>Hotspot is unsupported</source>
+        <translation>Hotspot is unsupported</translation>
+    </message>
+    <message>
+        <source>Personal Hotspot On</source>
+        <translation>Personal Hotspot On</translation>
+    </message>
+    <message>
+        <source>Personal Hotspot Off</source>
+        <translation>Personal Hotspot Off</translation>
+    </message>
+</context>
+<context>
+    <name>QuickPanel</name>
+    <message>
+        <source>HotSpot</source>
+        <translation>HotSpot</translation>
+    </message>
+</context>
+</TS>
---
+++ dock-hotspot-plugin/translations/dock-hotspot-plugin_zh_CN.ts
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>HotspotPlugin</name>
+    <message>
+        <source>hotspot</source>
+        <translation>热点</translation>
+    </message>
+    <message>
+        <source>Hotspot is unsupported</source>
+        <translation>不支持热点</translation>
+    </message>
+    <message>
+        <source>Personal Hotspot On</source>
+        <translation>个人热点开</translation>
+    </message>
+    <message>
+        <source>Personal Hotspot Off</source>
+        <translation>个人热点关</translation>
+    </message>
+</context>
+<context>
+    <name>QuickPanel</name>
+    <message>
+        <source>HotSpot</source>
+        <translation>热点</translation>
+    </message>
+</context>
+</TS>
