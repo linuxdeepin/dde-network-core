d3c7dc0636443d3d71802305ddad303293e33961
heyuming <heyuming@uniontech.com>
2023-07-05 11:50:11 +0800

    refactor: adjust source structure
    Log: 调整源代码结构

--- common-plugin/widgets/tipswidget.h
+++ common-plugin/widgets/tipswidget.h
@@ -1,11 +1,11 @@
-// SPDX-FileCopyrightText: 2022 UnionTech Software Technology Co., Ltd.
+// SPDX-FileCopyrightText: 2022 - 2023 UnionTech Software Technology Co., Ltd.
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 #ifndef TIPSWIDGET_H
 #define TIPSWIDGET_H
 
-#include "../utils.h"
+#include "../plugin_global.h"
 
 #include <QFrame>
 
