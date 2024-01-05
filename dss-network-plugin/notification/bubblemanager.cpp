fd91047b857d68689d2656f8abf36f1014b18e08
chenhongtao <chenhongtao@deepin.org>
2024-01-05 17:00:42 +0800

    chore: change license to lgpl
    Log:

--- dss-network-plugin/notification/bubblemanager.cpp
+++ dss-network-plugin/notification/bubblemanager.cpp
@@ -1,6 +1,6 @@
 ﻿// SPDX-FileCopyrightText: 2018 - 2022 UnionTech Software Technology Co., Ltd.
 //
-// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-License-Identifier: LGPL-3.0-or-later
 #include "bubblemanager.h"
 #include "bubble.h"
 #include "notificationentity.h"
