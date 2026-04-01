10e995e5acefedd68766cb933f25da4eb63298b6
donghualin <donghualin@uniontech.com>
2026-04-01 18:23:17 +0800

    fix: fix the language of the first connection which create
    fix the language of the first connection which create
    
    PMS: BUG-354915

--- network-service-plugin/src/system/networkinitialization.h
+++ network-service-plugin/src/system/networkinitialization.h
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2024 UnionTech Software Technology Co., Ltd.
+// SPDX-FileCopyrightText: 2024 - 2026 UnionTech Software Technology Co., Ltd.
 //
 // SPDX-License-Identifier: LGPL-3.0-or-later
 
@@ -23,40 +23,45 @@ class NetworkInitialization : public QObject
     Q_OBJECT
 
 public:
-    static void doInit();
-
-protected:
     explicit NetworkInitialization(QObject *parent = nullptr);
     ~NetworkInitialization() = default;
+    void updateLanguage(const QString &locale);
+
+protected:
     void initDeviceInfo();
     void initConnection();
 
 private:
     void addFirstConnection();
-    void addFirstConnection(const QSharedPointer<NetworkManager::WiredDevice> &device);
-    bool hasConnection(const QSharedPointer<NetworkManager::WiredDevice> &device, QList<QSharedPointer<NetworkManager::Connection>> &unSaveDevices);
-    QString connectionMatchName() const;
+    void addFirstConnection(NetworkManager::WiredDevice *device);
+    bool hasConnection(NetworkManager::WiredDevice *device, QList<QSharedPointer<NetworkManager::Connection>> &unSaveDevices);
+    QPair<int, QString> connectionMatchName(NetworkManager::WiredDevice *device) const;
+    QVariant accountInterface(const QString &path, const QString &key, bool isUser = true) const;
     bool installUserTranslator(const QString &json);
-    bool installTranslator(const QString &locale);
-    bool installSystemTranslator();
+    void installLaunguage(const QString &locale);
     void hideWirelessDevice(const QSharedPointer<NetworkManager::Device> &device, bool disableNetwork);
     void initDeviceConnection(const QSharedPointer<NetworkManager::WiredDevice> &device);
     void checkAccountStatus();
+    void updateConnectionLaunguage(const QString &account);
+    void updateConnectionLaunguage();
+    bool isServerSystem() const;
 
 private slots:
     void onUserChanged(const QString &json);
     void onUserAdded(const QString &json);
     void onInitDeviceConnection();
-    void onAddFirstConnection();
-    void onManagedChanged();
+
+    void onWiredDevicePropertyChanged();
+    void onDeviceAdded(const QString &uni);
+    void onAvailableConnectionDisappeared(const QString &connectionUni);
 
 private:
-    QStringList m_newConnectionNames;
+    QMap<QString, QString> m_newConnectionNames;
     bool m_initilized;
     bool m_accountServiceRegister;
     bool m_hasAddFirstConnection;
     QMap<QString, QDateTime> m_lastCreateTime;
-    QMap<QString, QSharedPointer<NetworkManager::Device>> m_devs;
+    QMap<QString, int> m_untranslactionConnections;
 };
 
 }
